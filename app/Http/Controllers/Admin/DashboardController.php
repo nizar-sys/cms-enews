21a3fbd366b30e7bf0d99033b00131cfcbcf73a6
nizar-sys <raspbery132@gmail.com>
2024-08-01 11:06:36 +0700

    rev backend (minus page builder)

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\News;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $data = [
+            'count_post' => Post::count(),
+            'count_news' => News::count(),
+            'count_article' => Article::count(),
+            'count_user' => User::count(),
+        ];
+
+        return view('admin.dashboard', compact('data'));
     }
 }
